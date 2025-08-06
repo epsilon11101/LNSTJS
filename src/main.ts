7db138e68111939c201f2729b5741c4ff3bfc166
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-08-06 00:45:00 -0600

    feat: add global validation pipe with transformation options

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // global validator PIPE
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true, // ← CRUCIAL
+      transformOptions: {
+        enableImplicitConversion: true, // ← convierte tipos (ej: string → UUID)
+        exposeUnsetFields: false, // ← opcional, para que no incluya undefined en la serialización
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
