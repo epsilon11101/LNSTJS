b47038baa4436b0b72b96b8242a93e787c2bc502
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-08-07 15:36:08 -0600

    feat: implement SeedController and SeedService for database seeding

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { CARS_SEED } from './data/cars.seed';
+import { BRANDS_SEED } from './data/brands.seed';
+import { CarsService } from '../cars/cars.service';
+import { BrandsService } from 'src/brands/brands.service';
+
+@Injectable()
+export class SeedService {
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) {}
+
+  populateDB() {
+    this.carsService.fillWithSeed(CARS_SEED);
+    this.brandsService.fillWithSeed(BRANDS_SEED);
+    return 'SEED EXECUTED!!!';
+  }
+}
