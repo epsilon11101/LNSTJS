f2384a35877c282bf337aa4115a00872bd9b57ed
epsilon11101 <28758777+epsilon11101@users.noreply.github.com>
2025-08-07 14:35:01 -0600

    feat: define CreateBrandDto and UpdateBrandDto with validation rules

--- src/brands/dto/update-brand.dto.ts
+++ src/brands/dto/update-brand.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBrandDto } from './create-brand.dto';
+import { IsOptional, IsString, IsUUID, MinLength } from 'class-validator';
 
-export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
+export class UpdateBrandDto extends PartialType(CreateBrandDto) {
+  @IsUUID()
+  @IsString()
+  @IsOptional()
+  readonly id: string;
+  @IsString()
+  @MinLength(3)
+  readonly name: string;
+}
